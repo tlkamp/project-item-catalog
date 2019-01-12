be00455d47d5133dbf5ee887b66e8ec040fb000e
Traci Kamp <traci.kamp@gmail.com>
2019-01-12 13:03:33 -0600

    Initial commit

---
+++ dbmodel.py
@@ -0,0 +1,67 @@
+from sqlalchemy import Column, String, ForeignKey, Integer, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    email = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    desc = Column(String(250))
+    last_updated = Column(DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    category = relationship(Category)
+    category_id = Column(Integer, ForeignKey('category.id'))
+
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.desc,
+            'last_updated': self.last_updated,  # TODO check formatting here
+            'category': self.category_id,
+            'user': self.user_id
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
