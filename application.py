1329a080a0da02035ca06c1651b887d25063bc9f
Traci Kamp <traci.kamp@gmail.com>
2019-01-12 16:05:49 -0600

    Add some basic routes to app.py

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 import flask
+from dbmodel import Item, Category
+from dbhelper import DBHelper
 
 app = flask.Flask(__name__)
 
@@ -7,6 +9,34 @@ app = flask.Flask(__name__)
 @app.route('/catalog')
 def default():
     return 'Hello World!'
+
+
+@app.route('/catalog/items/all')
+def show_all_items():
+    helper = DBHelper()
+    items = helper.session.query(Item).all()
+    return flask.jsonify({'items': [item.serialize for item in items]})
+
+
+@app.route('/catalog/items/<int:itemid>')
+def show_specific_item(itemid):
+    helper = DBHelper()
+    item = helper.session.query(Item).filter_by(id=itemid).one()
+    return flask.jsonify({'item': item.serialize})
+
+
+@app.route('/catalog/categories/all')
+def show_all_categories():
+    helper = DBHelper()
+    categories = helper.session.query(Category).all()
+    return flask.jsonify({'categories': [category.serialize for category in categories]})
+
+
+@app.route('/catalog/categories/<int:categoryid>')
+def show_specific_category(categoryid):
+    helper = DBHelper()
+    category = helper.session.query(Category).filter_by(id=categoryid).one()
+    return flask.jsonify({'category': category.serialize})
 
 
 if __name__ == "__main__":
