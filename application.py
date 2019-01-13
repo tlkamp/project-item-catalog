1d5794101e4fec56d6f7f372b509c75922271d34
Traci Kamp <traci.kamp@gmail.com>
2019-01-13 10:04:54 -0600

    Fix indentation and use kwargs

--- application.py
+++ application.py
@@ -8,36 +8,37 @@ app = flask.Flask(__name__)
 @app.route('/')
 @app.route('/catalog')
 def default():
-  helper = DBHelper()
-  categories = helper.session.query(Category).all()
-  return flask.render_template('index.html', categories=categories)
+    helper = DBHelper()
+    categories = helper.session.query(Category).all()
+    return flask.render_template('index.html', categories=categories)
+
 
 @app.route('/catalog/items/all')
 def show_all_items():
     helper = DBHelper()
     items = helper.session.query(Item).all()
-    return flask.jsonify({'items': [item.serialize for item in items]})
+    return flask.jsonify(items=[item.serialize for item in items])
 
 
 @app.route('/catalog/items/<int:itemid>')
 def show_specific_item(itemid):
     helper = DBHelper()
     item = helper.session.query(Item).filter_by(id=itemid).one()
-    return flask.jsonify({'item': item.serialize})
+    return flask.jsonify(item=item.serialize)
 
 
 @app.route('/catalog/categories/all')
 def show_all_categories():
     helper = DBHelper()
     categories = helper.session.query(Category).all()
-    return flask.jsonify({'categories': [category.serialize for category in categories]})
+    return flask.jsonify(categories=[category.serialize for category in categories])
 
 
 @app.route('/catalog/categories/<int:categoryid>')
 def show_specific_category(categoryid):
     helper = DBHelper()
     category = helper.session.query(Category).filter_by(id=categoryid).one()
-    return flask.jsonify({'category': category.serialize})
+    return flask.jsonify(category=category.serialize)
 
 
 if __name__ == "__main__":
