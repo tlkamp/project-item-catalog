f41b69ab4fbdb041993951d812d9e57d4eff0caf
Traci Kamp <traci.kamp@gmail.com>
2019-01-19 08:45:58 -0600

    remove the old references to the user email in dbhelper

--- dbhelper.py
+++ dbhelper.py
@@ -13,8 +13,8 @@ class DBHelper(object):
     def __init__(self):
         self.session = DBHelper.__DBSession()
 
-    def create_item(self, name, description, category_name, user_email):
-        current_user = self.session.query(User).filter_by(email=user_email).one()
+    def create_item(self, name, description, category_name, username):
+        current_user = self.session.query(User).filter_by(name=username).one()
         if not self.category_exists(category_name):
             self.create_category(category_name)
 
@@ -35,15 +35,15 @@ class DBHelper(object):
         self.session.add(new_category)
         self.session.commit()
 
-    def create_user(self, username, user_email):
-        if not self.user_exists(user_email):
-            new_user = User(name=username, email=user_email)
+    def create_user(self, username):
+        if not self.user_exists(username):
+            new_user = User(name=username)
             self.session.add(new_user)
             self.session.commit()
 
-    def user_exists(self, user_email):
+    def user_exists(self, username):
         try:
-            self.session.query(User).filter_by(email=user_email).one()
+            self.session.query(User).filter_by(name=username).one()
             return True
         except NoResultFound:
             return False
@@ -54,12 +54,13 @@ class DBHelper(object):
 
 
 if __name__ == "__main__":
+    default_user = 'default_user'
     helper = DBHelper()
-    helper.create_user('default_user', 'default@example.com')
+    helper.create_user(default_user)
     pprint([user.serialize for user in helper.session.query(User).all()])
 
-    helper.create_item('default item', 'something to test with', 'default', 'default@example.com')
-    helper.create_item('another default', 'something with a later modified date', 'default', 'default@example.com')
+    helper.create_item('default item', 'something to test with', 'default', default_user)
+    helper.create_item('another default', 'something with a later modified date', 'default', default_user)
 
     pprint([category.serialize for category in helper.session.query(Category).all()])
     pprint([item.serialize for item in helper.session.query(Item).all()])
